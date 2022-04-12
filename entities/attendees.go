c25e20bd59777fe624492520c196ff5182302902
Usamah Abdurrahman <abdurrahman.usamah07@gmail.com>
2022-04-12 10:44:18 +0700

    update skeleton

--- entities/attendees.go
+++ entities/attendees.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type Attendees struct {
 	gorm.Model
-	EventId           uint `gorm:"not null" json:"event_id" form:"event_id"`
-	UserId            uint `gorm:"not null" json:"user_id" form:"user_id"`
-	TotalParticipants uint `gorm:"not null" json:"total_participants" form:"total_participants"`
-	User              User `gorm:"foreignKey:UserId;references:ID"`
+	EventId uint `gorm:"not null" json:"event_id" form:"event_id"`
+	UserId  uint `gorm:"not null" json:"user_id" form:"user_id"`
+	User    User `gorm:"foreignKey:UserId;references:ID"`
 }
