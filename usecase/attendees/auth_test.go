f77a8e7125cd4fd46ad81083bfede50db73a434e
satriacening <satriacening@gmail.com>
2022-04-18 00:45:02 +0700

    add unit test for usecase.attendees

--- usecase/attendees/auth_test.go
+++ usecase/attendees/auth_test.go
@@ -1,52 +1,117 @@
 package attendees
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	_entities "group-project/dolan-planner/entities"
 
-// func TestPostAttendees(t *testing.T) {
-// 	t.Run("TestPostAttendeesSuccess", func(t *testing.T) {
-// 		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepository{})
-// 		data, idErr, err := attendeesUseCase.PostAttendees(idToken, idEvent)
-// 		assert.Equal(t, "Token", data)
-// 		assert.Nil(t, err)
-// 	})
+	"github.com/stretchr/testify/assert"
+)
 
-// 	t.Run("TestPostAttendeesError", func(t *testing.T) {
-// 		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepositoryError{})
-// 		data, err := attendeesUseCase.PostAttendees(1, 2)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, "", data)
-// 	})
-// }
+func TestPostAttendees(t *testing.T) {
+	t.Run("TestPostAttendeesSuccess", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepository{})
+		attendees, err := attendeesUseCase.PostAttendees(1, 1)
+		assert.Equal(t, _entities.Attendees{
+			EventId: 1, UserId: 1,
+		}, attendees)
+		assert.Nil(t, err)
+	})
 
-// type mockAttendeesRepository struct{}
+	t.Run("TestPostAttendeesError", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepositoryError{})
+		_, err := attendeesUseCase.PostAttendees(1, 2)
+		assert.NotNil(t, err)
+	})
+}
+func TestGetAttendees(t *testing.T) {
+	t.Run("TestPostAttendeesSuccess", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepository{})
+		attendees, rows, err := attendeesUseCase.GetAttendees(1)
+		assert.Equal(t, _entities.Attendees{
+			EventId: 1, UserId: 1,
+		}, attendees)
+		assert.Equal(t, 1, rows)
+		assert.Nil(t, err)
+	})
 
-// func (m mockAttendeesRepository) PostAttendees(idEvent uint, idToken uint) (string, error) {
-// 	return "Token", nil
-// }
+	t.Run("TestPostAttendeesError", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepositoryError{})
+		_, _, err := attendeesUseCase.GetAttendees(1)
+		assert.NotNil(t, err)
+	})
+}
+func TestDeleteAttendees(t *testing.T) {
+	t.Run("TestPostAttendeesSuccess", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepository{})
+		rows, err := attendeesUseCase.DeleteAttendees(1, 1)
+		assert.Equal(t, 1, rows)
+		assert.Nil(t, err)
+	})
 
-// func (m mockAttendeesRepository) GetAttendees(idEvent uint, idToken uint) (string, error) {
-// 	return "Token", nil
-// }
+	t.Run("TestPostAttendeesError", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepositoryError{})
+		_, err := attendeesUseCase.DeleteAttendees(1, 1)
+		assert.NotNil(t, err)
+	})
+}
+func TestGetAttendeesUser(t *testing.T) {
+	t.Run("TestPostAttendeesSuccess", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepository{})
+		attendees, rows, err := attendeesUseCase.GetAttendeesUser(1)
+		assert.Equal(t, _entities.Attendees{
+			EventId: 1, UserId: 1,
+		}, attendees)
+		assert.Equal(t, 1, rows)
+		assert.Nil(t, err)
+	})
 
-// func (m mockAttendeesRepository) DeleteAttendees(idEvent uint, idToken uint) (string, error) {
-// 	return "Token", nil
-// }
+	t.Run("TestPostAttendeesError", func(t *testing.T) {
+		attendeesUseCase := NewAttendeesUseCase(mockAttendeesRepositoryError{})
+		_, _, err := attendeesUseCase.GetAttendeesUser(1)
+		assert.NotNil(t, err)
+	})
+}
 
-// type mockAttendeesRepositoryError struct{}
+type mockAttendeesRepository struct{}
 
-// func (m mockAttendeesRepositoryError) PostAttendees(email string, password string) (string, error) {
-// 	return "", fmt.Errorf("Unautorizad")
-// }
+func (m mockAttendeesRepository) PostAttendees(idEvent uint, idToken uint) (_entities.Attendees, error) {
+	return _entities.Attendees{
+		EventId: 1, UserId: 1,
+	}, nil
+}
 
-// func (m mockAttendeesRepositoryError) GetAttendees(email string, password string) (string, error) {
-// 	return "", fmt.Errorf("Unautorizad")
-// }
+func (m mockAttendeesRepository) GetAttendees(idEvent uint) ([]_entities.Attendees, int, error) {
+	return []_entities.Attendees{
+		{EventId: 1, UserId: 1},
+	}, 1, nil
+}
 
-// func (m mockAttendeesRepositoryError) DeleteAttendees(email string, password string) (string, error) {
-// 	return "", fmt.Errorf("Unautorizad")
-// }
+func (m mockAttendeesRepository) DeleteAttendees(idToken uint, idEvent uint) (uint, error) {
+	return 1, nil
+}
+
+func (m mockAttendeesRepository) GetAttendeesUser(idToken uint) ([]_entities.Attendees, int, error) {
+	return []_entities.Attendees{
+		{EventId: 1, UserId: 1},
+	}, 1, nil
+}
+
+type mockAttendeesRepositoryError struct{}
+
+func (m mockAttendeesRepositoryError) PostAttendees(idEvent uint, idToken uint) (_entities.Attendees, error) {
+	return _entities.Attendees{}, fmt.Errorf("failed to post attendees")
+}
+
+func (m mockAttendeesRepositoryError) GetAttendees(idEvent uint) ([]_entities.Attendees, int, error) {
+	return nil, 0, fmt.Errorf("failed to get attendes")
+}
+
+func (m mockAttendeesRepositoryError) DeleteAttendees(idToken uint, idEvent uint) (uint, error) {
+	return 0, fmt.Errorf("failed to delete attendes")
+}
+
+func (m mockAttendeesRepositoryError) GetAttendeesUser(idToken uint) ([]_entities.Attendees, int, error) {
+	return nil, 1, fmt.Errorf("failed to get attendes by user")
+}
